7b9a6f00d40a3a72cb1fc7d31e2e46d8a9b78226
Princeyadav1611 <princeyadav161101@gmail.com>
2026-09-14 12:37:30 +0530

    Removing ScrollBar

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -1,4 +1,7 @@
 const Section2 = () => {
-  return <div className="h-screen w-full bg-gray-300">Fucking Hell</div>;
+  return (
+  <div className="h-screen w-full bg-gray-300">
+    
+  </div>)
 };
 export default Section2;
