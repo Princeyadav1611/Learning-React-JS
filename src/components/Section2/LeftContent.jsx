448f212aec45d994475aff54eb0ce75cd12bdd3b
Princeyadav1611 <princeyadav161101@gmail.com>
2026-09-15 07:22:38 +0530

    improving it

--- src/components/Section2/LeftContent.jsx
+++ src/components/Section2/LeftContent.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 
 const Form = () => {
-  const[title, setTitle] = useState("");
-  const[desc, setDesc] = useState("");
-   const[task, setTask] = useState([]);
-   const copyTask = [...task];
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [task, setTask] = useState([]);
 
   return (
     <div className="flex flex-col justify-around">
@@ -22,15 +21,25 @@ const Form = () => {
             <path
               id="Union"
               fill="#000000"
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M1.5479 0.24H0.2833v22.7614h9.3575v-2.5291H2.8124V2.769h13.6568v8.3533l2.5291 -2.5291V0.24H1.5479Zm4.0464 5.311h8.093v4.0465h-8.093V5.551Zm13.1511 6.1564 0.7153 0.7153 3.5406 3.5407 0.7153 0.7153 -0.7153 0.7153 -6.0697 6.0697 -0.2963 0.2963H11.664v-4.9713l0.2963 -0.2963 6.0697 -6.0697 0.7154 -0.7153Z"
-              clip-rule="evenodd"
-              stroke-width="1"
+              clipRule="evenodd"
+              strokeWidth="1"
             ></path>
           </g>
         </svg>
       </h1>
-      <form className="flex translate-x-4 flex-col bg-white/80 border border-stone-200 shadow-sm rounded-2xl p-4">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const copyTask = [...task];
+          copyTask.push({title,desc });
+          setTask(copyTask);
+          setTitle("");
+          setDesc("");
+        }}
+        className="flex translate-x-4 flex-col bg-white/80 border border-stone-200 shadow-sm rounded-2xl p-4"
+      >
         <input
           type="text"
           placeholder="EnterTitle of Your Note"
@@ -49,14 +58,7 @@ const Form = () => {
           className=" h-100 bg-stone-50 text-stone-800 placeholder-stone-400 border-stone-300 focus:border-stone-300 p-6 text-xl font-medium m-1 rounded border-2 "
         ></textarea>
         <button
-          onClick={(e) => {
-            e.preventDefault();
-            setTitle("");
-            setDesc("");
-            copyTask.push({title,desc});
-            setTask(copyTask);
-            console.log(task);
-          }}
+          type="submit"
           className="p-3 m-2 active:scale-95 rounded bg-stone-900 hover:bg-black text-stone-100 cursor-pointer text-3xl font-extrabold "
         >
           Add Note
